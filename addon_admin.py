60c60ac799c930a0d62a2a73d71da743196fe6af
Callum Bartlett <coolum444@hotmail.co.uk>
2016-10-02 21:44:35 +0100

    Emoji addition function added Nonfunctional, due to Discord not allowing bots to upload emoji

--- addon_admin.py
+++ addon_admin.py
@@ -5,6 +5,7 @@ import os
 import sys
 import strawpoll
 from isAllowed import *
+from urllib.request import urlretrieve
 
 
 notallowed = "You are not allowed to use that command."
@@ -19,7 +20,7 @@ class Admin():
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(pass_context=True)
+    @commands.command(pass_context=True)
     async def ban(self, ctx):
         """Ban command."""
         if allowUse(ctx, ['ban']) == False:
@@ -32,7 +33,7 @@ class Admin():
             await self.bot.send_message(server, "**{}** has been banned.".format(ctx.message.mentions[0]))
         await self.bot.ban(ctx.message.mentions[0])
 
-    @commands.group(pass_context=True)
+    @commands.command(pass_context=True)
     async def kick(self, ctx):
         """Ban command."""
         if allowUse(ctx, ['kick']) == False:
@@ -44,6 +45,27 @@ class Admin():
             server = discord.Object(i['Bans']['Channel'])
             await self.bot.send_message(server, "**{}** has been kicked.".format(ctx.message.mentions[0]))
         await self.bot.kick(ctx.message.mentions[0])
+
+
+    @commands.group(pass_context=True)
+    async def emoji(self, ctx):
+        """Parent command for emoji usage"""
+        pass
+
+
+    @emoji.command(pass_context=True,name='add')
+    async def emojiAdd(self, ctx):
+        if allowUse(ctx, ['emoji']):
+            emojiName = ctx.message.content.split(' ',3)[2]
+            imgUrl = ctx.message.content.split(' ',3)[3]
+            urlretrieve(imgUrl, 'emojiTEMP.png')
+            a = open('emojiTEMP.png', 'rb')
+            b = a.read()
+            a.close()
+            await self.bot.create_custom_emoji(server=ctx.message.server, name=emojiName, image=b)
+            await self.bot.say("This emoji has been added.")
+        else:
+            await self.bot.say(notallowed)
 
 
 def setup(bot):
