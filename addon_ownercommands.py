97dbab48f61e24dad954e99b053c7df5450d40cd
Callum Bartlett <coolum444@hotmail.co.uk>
2016-10-03 04:32:02 +0100

    Added game command
    Can now change the game the bot is playing
    Also converted tabs to spaces because Python sucks

--- addon_ownercommands.py
+++ addon_ownercommands.py
@@ -3,98 +3,114 @@ from discord.ext import commands
 from isAllowed import *
 import requests
 import os
+import sys
 
 
 class OwnerCommands():
 
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
 
-	## Change the icon of the bot
-	@commands.command(pass_context=True)
-	async def av(self, ctx):
-	    if allowUse(ctx, ['is_caleb']):
-	        try:
-	        	## Get the url of the image
-	            a = requests.get(ctx.message.content.split(' ', 1)[1]).content
+    ## Change the icon of the bot
+    @commands.command(pass_context=True)
+    async def av(self, ctx):
+        if allowUse(ctx, ['is_caleb']):
+            try:
+                ## Get the url of the image
+                a = requests.get(ctx.message.content.split(' ', 1)[1]).content
 
-	            ## Change the avatar
-	            await self.bot.edit_profile(avatar=a)
-	            await self.bot.say("Changed profile image.")
-	        except Exception as e:
+                ## Change the avatar
+                await self.bot.edit_profile(avatar=a)
+                await self.bot.say("Changed profile image.")
+            except Exception as e:
 
-	        	## Work out what went wrong
-	            exc = '{}: {}'.format(type(e).__name__, e)
-	            await self.bot.say("Something went wrong :: {}".format(exc))
-	    else:
-	    	## Tell people when they aren't allowed
-	        await self.bot.say(notallowed)
-
-
-	## Restart any running instance of the bot
-	@commands.command(pass_context=True, aliases=['rs'])
-	async def restart(self, ctx):
-	    if allowUse(ctx, ['is_caleb']):
-	    	## Write to file the channel that needs to be pinged when online again
-	        with open(workingDirectory + 'restartFile.txt', 'w') as a:
-	            a.write(str(ctx.message.channel.id))
-	        await self.bot.say("Restarting...")
-
-	        ## os restart
-	        os.execl(sys.executable, *([sys.executable] + sys.argv))
-	    else:
-	        await self.bot.say(notallowed)
-	    return
+                ## Work out what went wrong
+                exc = '{}: {}'.format(type(e).__name__, e)
+                await self.bot.say("Something went wrong :: {}".format(exc))
+        else:
+            ## Tell people when they aren't allowed
+            await self.bot.say(notallowed)
 
 
-	## Kill all running instances of the bot
-	@commands.command(pass_context=True, aliases=['k'])
-	async def kill(self, ctx):
-	    if allowUse(ctx, ['is_caleb']):
-	    	## Exit using sys
-	        await self.bot.say("Killing.")
-	        sys.exit()
-	    else:
-	        await self.bot.say(notallowed)
+    ## Restart any running instance of the bot
+    @commands.command(pass_context=True, aliases=['rs'])
+    async def restart(self, ctx):
+        if allowUse(ctx, ['is_caleb']):
+            ## Write to file the channel that needs to be pinged when online again
+            with open(workingDirectory + 'restartFile.txt', 'w') as a:
+                a.write(str(ctx.message.channel.id))
+            await self.bot.say("Restarting...")
+
+            ## os restart
+            os.execl(sys.executable, *([sys.executable] + sys.argv))
+        else:
+            await self.bot.say(notallowed)
+        return
 
 
-	## Run any given line of code
-	@commands.command(pass_context=True, hidden=True)
-	async def ex(self, ctx):
-	    if allowUse(ctx, ['is_caleb']):
-	        exec(ctx.message.content.split(' ',1)[1])
-	    else:
-	        await self.bot.say(notallowed)
+    ## Kill all running instances of the bot
+    @commands.command(pass_context=True, aliases=['k'])
+    async def kill(self, ctx):
+        if allowUse(ctx, ['is_caleb']):
+            ## Exit using sys
+            await self.bot.say("Killing.")
+            sys.exit()
+        else:
+            await self.bot.say(notallowed)
 
 
-	## Reload an extention without restarting bot
-	@commands.command(aliases=["rldext"],pass_context=True)
-	async def reloadextension(self, ctx, *, ext: str=None):
-	    """Reload bot extension"""
-	    if allowUse(ctx, ['is_caleb']):
-	    	## Check there's an extention being asked about
-	        if ext == None:
-	            await self.bot.say("Please choose an extension, currently available to be reloaded are:\n```" + "\n".join(bot.cogs) + "```")
-	            return
+    ## Run any given line of code
+    @commands.command(pass_context=True, hidden=True)
+    async def ex(self, ctx):
+        if allowUse(ctx, ['is_caleb']):
+            exec(ctx.message.content.split(' ',1)[1])
+        else:
+            await self.bot.say(notallowed)
 
-	        await self.bot.say("Reloading extension...")
-	        try: 
-	        	## Unload it
-	        	self.bot.unload_extension(ext)
-	        except: 
-	        	pass
 
-	        try: 
-	        	## Load it
-	        	self.bot.load_extension(ext)
-	        except:
-	            await bot.say("That extention does not exist.")
-	            return
+    ## Reload an extention without restarting bot
+    @commands.command(aliases=["rldext"],pass_context=True)
+    async def reloadextension(self, ctx, *, ext: str=None):
+        """Reload bot extension"""
+        if allowUse(ctx, ['is_caleb']):
+            ## Check there's an extention being asked about
+            if ext == None:
+                await self.bot.say("Please choose an extension, currently available to be reloaded are:\n```" + "\n".join(bot.cogs) + "```")
+                return
 
-	        await self.bot.say("Done!")
-	    else:
-	        await self.bot.say(notallowed)
+            await self.bot.say("Reloading extension...")
+            try: 
+                ## Unload it
+                self.bot.unload_extension(ext)
+            except: 
+                pass
+
+            try: 
+                ## Load it
+                self.bot.load_extension(ext)
+            except:
+                await self.bot.say("That extention does not exist.")
+                return
+
+            await self.bot.say("Done!")
+        else:
+            await self.bot.say(notallowed)
+
+
+    ## Changes the "playing" status of the bot
+    @commands.command(pass_context=True)
+    async def game(self, ctx):
+        """Change the bot's game"""
+        if allowUse(ctx, ['is_caleb']):
+            try:
+                gameTo = discord.Game(name=ctx.message.content.split(' ',1)[1])
+            except IndexError:
+                gameTo = None 
+            await self.bot.change_presence(game=gameTo)
+            await self.bot.say("Presence has been updated.")
+        else:
+            await self.bot.say(notallowed)
 
 
 def setup(bot):
