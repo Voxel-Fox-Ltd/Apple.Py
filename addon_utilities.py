ab50ecb455a9875eb61ba0f5b8fdd179bcee93c7
Callum Bartlett <coolum444@hotmail.co.uk>
2016-10-15 19:06:46 +0100

    Clean no longer uses purge_from
    This was a workaround for some slow API calls. This will return to be
    optional later.

--- addon_utilities.py
+++ addon_utilities.py
@@ -10,7 +10,7 @@ class Utilities:
 
 
     def is_bot(self, m):
-        if m.author == self.bot.user or (m.content[0] == '.' and m.content[1].lower() in 'abcdefghijklmnopqrstuvwxyz'):
+        if m.author == self.bot.user:
             if m.content.startswith('Cleaned up'):
                 return False
             else:
@@ -40,15 +40,28 @@ class Utilities:
     @commands.command(pass_context=True, help=helpText['clean'][1], brief=helpText['clean'][0])
     async def clean(self, ctx):
         """Deletes the bot's messages from the last 50 posted to the channel."""
+        counter = 0
         try:
             if ctx.message.content.split(' ')[1].lower() == 'true':
-                q = await self.bot.purge_from(ctx.message.channel, limit=50, check=self.is_bot_no_limit)
+                # q = await self.bot.purge_from(ctx.message.channel, limit=50, check=self.is_bot_no_limit)
+                async for i in self.bot.logs_from(ctx.message.channel, limit=50):
+                    try:
+                        if i.author == self.bot.user or (i.content[0] == '.' and i.content[1].lower() in 'abcdefghijklmnopqrstuvwxyz'):
+                            await self.bot.delete_message(i)
+                            counter += 1
+                    except:
+                        pass
+                await self.bot.say("Cleaned up **{}** messages".format(counter))
                 return
         except:
             pass
 
-        q = await self.bot.purge_from(ctx.message.channel, limit=50, check=self.is_bot)
-        await self.bot.say("Cleaned up **{}** messages".format(len(q)))
+        # q = await self.bot.purge_from(ctx.message.channel, limit=50, check=self.is_bot)
+        async for i in self.bot.logs_from(ctx.message.channel, limit=50):
+            if i.author.id == self.bot.user.id:
+                await self.bot.delete_message(i)
+                counter += 1
+        await self.bot.say("Cleaned up **{}** messages".format(counter))
 
 
 def setup(bot):
