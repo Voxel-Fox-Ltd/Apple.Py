5f4d6b78d72ec96345e875193c224543cc7068d2
Caleb Bartlett <callum@voxelfox.co.uk>
2020-02-21 00:01:33 +0000

    Fix avatar command for users

--- cogs/user_info.py
+++ cogs/user_info.py
@@ -9,18 +9,13 @@ from cogs import utils
 class UserInfo(utils.Cog):
 
     @commands.command(cls=utils.Command)
-    async def avatar(self, ctx:utils.Context, user:typing.Optional[discord.User]=None):
+    async def avatar(self, ctx:utils.Context, user:discord.User=None):
         """Shows you the avatar of a given user"""
 
         if user is None:
             user = ctx.author
-        colour = None
-        if ctx.guild:
-            colour = ctx.guild.get_member(user.id).top_role.colour
-        with utils.Embed(colour=colour) as embed:
+        with utils.Embed(use_random_colour=True) as embed:
             embed.set_image(url=user.avatar_url)
-        if colour is None:
-            embed.use_random_colour()
         await ctx.send(embed=embed)
 
 
