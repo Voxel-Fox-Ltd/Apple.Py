1ac3de8f9af7ae5a43f8639a839b0896ad32babe
Caleb Bartlett <callum@voxelfox.co.uk>
2020-06-05 21:44:43 +0100

    Add charinfo command

--- cogs/misc_commands.py
+++ cogs/misc_commands.py
@@ -3,6 +3,7 @@ import typing
 
 import discord
 from discord.ext import commands
+import unicodedata
 
 from cogs import utils
 
@@ -93,6 +94,23 @@ class MiscCommands(utils.Cog):
         with utils.Embed(colour=hex_colour,title=f"#{hex_colour:0>6X}") as embed:
             embed.set_image(url=f"https://www.htmlcsscolor.com/preview/gallery/{hex_colour:0>6X}.png")
         await ctx.send(embed=embed)
+
+    @commands.command(cls=utils.Command)
+    @commands.bot_has_permissions(send_messages=True)
+    async def charinfo(self, ctx, *, characters: str):
+        """Shows you information about a number of characters.
+
+        Only up to 25 characters at a time.
+        """
+
+        def to_string(c):
+            digit = f'{ord(c):x}'
+            name = unicodedata.name(c, 'Name not found.')
+            return f'`\\U{digit:>08}`: {name} - {c} \N{EM DASH} <http://www.fileformat.info/info/unicode/char/{digit}>'
+        msg = '\n'.join(map(to_string, characters))
+        if len(msg) > 2000:
+            return await ctx.send('Output too long to display.')
+        await ctx.send(msg)
 
 
 def setup(bot:utils.Bot):
