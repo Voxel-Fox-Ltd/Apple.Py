424f352ce3f4fd8f1ea4e8d6810a8b5ecdd9b511
Callum Bartlett <callum@voxelfox.co.uk>
2020-08-07 20:52:30 +0100

    Let stealemoji work via url

--- cogs/emoji_commands.py
+++ cogs/emoji_commands.py
@@ -1,4 +1,5 @@
 import typing
+import re
 
 import discord
 from discord.ext import commands
@@ -6,25 +7,48 @@ from discord.ext import commands
 from cogs import utils
 
 
+class ImageUrl(commands.Converter):
+
+    regex = re.compile(r"(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)")
+
+    async def convert(self, ctx:utils.Context, argument:str):
+        """Make sure a given string argument is an image URL"""
+
+        v = self.regex.search(argument)
+        if v is None:
+            raise commands.BadArgument()
+        return argument
+
+
 class EmojiCommands(utils.Cog):
 
-    @commands.command(cls=utils.Command)
+    @commands.command(cls=utils.Command, aliases=['addemoji'])
     @commands.bot_has_permissions(manage_emojis=True)
     @commands.has_guild_permissions(manage_emojis=True)
     @commands.guild_only()
-    async def stealemoji(self, ctx:utils.Context, emoji:typing.Union[discord.PartialEmoji, int], name:str=None, animated:bool=False):
+    async def stealemoji(self, ctx:utils.Context, emoji:typing.Union[discord.PartialEmoji, int, ImageUrl], name:str=None, animated:bool=False):
         """Copies an emoji and uploads it to your server"""
 
+        # See if we gave an emoji ID
         if isinstance(emoji, int):
             if name is None:
                 raise utils.errors.MissingRequiredArgumentString("name")
             emoji = discord.PartialEmoji(name=name, animated=animated, id=emoji)
 
-        url = str(emoji.url)
+        # See if we gave an image URL or an emoji
+        if isinstance(emoji, discord.PartialEmoji):
+            url = str(emoji.url)
+            name = name or emoji.name
+        else:
+            url = emoji
+
+        # Grab image data
         async with self.bot.session.get(url) as r:
             data = await r.read()
+
+        # Upload that to Discord
         try:
-            e = await ctx.guild.create_custom_emoji(name=name or emoji.name, image=data)
+            e = await ctx.guild.create_custom_emoji(name=name, image=data)
         except discord.HTTPException as e:
             return await ctx.send(f"I couldn't create that emoji - {e}")
         await ctx.send(f"Emoji added - {e!s}")
