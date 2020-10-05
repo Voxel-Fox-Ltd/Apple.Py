f57991d1e5a07a17bc229e76449b4c3eca534b69
Caleb Bartlett <coolum444@hotmail.co.uk>
2020-10-05 05:54:26 +0100

    Gently move around some variables

--- cogs/useridD_pinger.py
+++ cogs/useridD_pinger.py
@@ -8,10 +8,8 @@ from cogs import utils
 class UserIDPinger(utils.Cog):
 
     GUILD_ID = 208895639164026880
-
-    ID_IN_MESSAGE_MATCHER = re.compile(r"(?<!<@)[0-9]{15,23}(?!>)$")
-
     STAFF_COMMANDS_CHANNEL_ID = 528827626907762689
+    ID_IN_MESSAGE_MATCHER = re.compile(r"(?<!<@)(?P<uid>[0-9]{15,23})(?!>)$")
 
     @utils.Cog.listener()
     async def on_message(self, message:discord.Message):
@@ -26,13 +24,10 @@ class UserIDPinger(utils.Cog):
         m = self.ID_IN_MESSAGE_MATCHER.search(message.content)
         if m is None:
             return
-        userid = m.group()
+        user_id = m.group("uid")
 
         # Send the ping-y boi
-        try:
-          await ctx.send(f"{self.bot.get_user(int(userid)).mention}")
-        except AttributeError:
-          return
+        await ctx.send(f"<@{user_id}>")
 
 
 def setup(bot:utils.Bot):
