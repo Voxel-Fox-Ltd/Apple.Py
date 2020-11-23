0cdbad3a9ae68812aa9d30343032f6e58fe3e4a1
Callum Bartlett <callum@voxelfox.co.uk>
2020-11-23 19:04:28 +0000

    Automate the Dialogflow API key refreshing

--- cogs/marriagebot_support.py
+++ cogs/marriagebot_support.py
@@ -1,3 +1,5 @@
+from datetime import datetime as dt, timedelta
+
 import discord
 import voxelbotutils as utils
 
@@ -10,6 +12,27 @@ class MarriageBotHelper(utils.Cog):
     DIALOGFLOW_URL = "https://dialogflow.clients6.google.com/v2/projects/voxel-fox/agent/sessions/b40c7c12-4b0f-41a5-2537-71d84e6489ee:detectIntent"
     DISOWN_USER_INTENT = "projects/voxel-fox/agent/intents/b9d0a9d5-acce-4f64-97c7-0e3c3e39fe22"
 
+    def __init__(self, bot:utils.Bot):
+        super().__init__(bot)
+        self.api_key = None
+
+    async def get_access_token(self):
+        """
+        Get the Dialogflow access token for the bot
+        """
+
+        # See if we have an existing valid token
+        if self.api_key is not None and self.api_key[1] < dt.utcnow() - timedelta(minutes=1):
+            return self.api_key[0]
+
+        # Alright sick let's get a new one
+        json = self.bot.config.get('api_keys', {}).get('dialogflow', {})
+        json.update({"grant_type": "refresh_token"})
+        async with self.bot.session.post("https://oauth2.googleapis.com/token", json=json) as r:
+            data = await r.json()
+        self.api_key = (data['access_token'], dt.utcnow() + timedelta(seconds=data['expires_in']))
+        return self.api_key[0]
+
     @utils.Cog.listener()
     async def on_message(self, message:discord.Message):
         """
@@ -18,7 +41,7 @@ class MarriageBotHelper(utils.Cog):
         """
 
         # Make sure we have an API key
-        if not self.bot.config.get('api_keys', {}).get('dialogflow'):
+        if not self.bot.config.get('api_keys', {}).get('dialogflow', {}):
             return
 
         # Make sure it's in the right place
@@ -45,7 +68,7 @@ class MarriageBotHelper(utils.Cog):
             },
         }
         headers = {
-            "Authorization": f"Bearer {self.bot.config['api_keys']['dialogflow']}",
+            "Authorization": f"Bearer {await self.get_access_token()}",
         }
         async with self.bot.session.post(self.DIALOGFLOW_URL, json=json, headers=headers) as r:
             if r.status != 200:
