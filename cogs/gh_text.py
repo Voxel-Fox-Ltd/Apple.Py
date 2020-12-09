e9f66b8701deec6ecccf3d1a6b0327dccd2f77d9
Callum Bartlett <callum@voxelfox.co.uk>
2020-12-09 01:07:27 +0000

    Rename GHText cog

--- cogs/gh_text.py
+++ cogs/gh_text.py
@@ -1,7 +1,7 @@
 import re
 
 import voxelbotutils as utils
-import discord
+
 
 class GHText(utils.Cog):
 
@@ -10,19 +10,19 @@ class GHText(utils.Cog):
         """
         Sends GitHub links if a message sent in the server matches the format `gh/user/repo`.
         """
-        
+
         # Find matches in the message
         m = re.finditer(r'gh/(?P<url>(?P<user>\S{1,39})/(?P<repo>\S{1,100}))', message.content)
-        
+
         # Add the url of each matched link to the final output
         sendable = ""
         for i in m:
             url = i.group("url")
             sendable += f"<https://github.com/{url}>\n"
-        
+
         # Send the GitHub links if there's any output
         if sendable:
-            await ctx.send(sendable)
+            await message.channel.send(sendable)
 
 
 def setup(bot:utils.Bot):
