8eae9d585a5d61bb3138cc5a04b0647bd5115a69
Callum Bartlett <callum@voxelfox.co.uk>
2020-12-09 04:34:42 +0000

    Attempt to add the JDA docs to the RTFM command

--- cogs/library_docs.py
+++ cogs/library_docs.py
@@ -46,6 +46,10 @@ class LibraryDocs(utils.Cog):
 
     DISCORDJS_DOCS = None
 
+    def __init__(self, bot:utils.Bot):
+        super().__init__(bot)
+        self._rtfm_cache = {}  # {library: {class_or_method: url_to_doc}}
+
     @utils.Cog.listener()
     async def on_message(self, message:discord.Message):
         """
@@ -61,13 +65,66 @@ class LibraryDocs(utils.Cog):
         except Exception:
             pass
 
+    def get_embed_from_cache(self, cache, obj) -> dict:
+        """
+        Build up some kwargs from the cache and a given input object.
+        """
+
+        item_casefold = obj.casefold()
+        split = item_casefold.split('.')
+        outputs = []
+
+        for key, link in cache.items():
+
+            # Key would be something like 'TextChannel.fetch', now 'textchannel.fetch'
+            key_casefold = key.casefold()
+
+            # See if we got an exact match
+            if item_casefold in key_casefold:
+                if item_casefold == key_casefold:
+                    outputs.append((key, link, 20,))
+                else:
+                    outputs.append((key, link, 15,))
+
+            # See if we're looking for a method
+            if len(split) == 1:
+                continue
+
+            # Search by method
+            if split[1] and split[1] in key_casefold:
+                if split[1] == key_casefold:
+                    outputs.append((key, link, 12,))
+                else:
+                    outputs.append((key, link, 10,))
+
+            # Search by class
+            if split[0] and split[0] in key_casefold:
+                if split[0] == key_casefold:
+                    outputs.append((key, link, 7,))
+                else:
+                    outputs.append((key, link, 5,))
+
+        # Hey maybe they didn't say anything useful
+        if len(outputs) == 0:
+            return {"content": "I couldn't find anything. Sorry."}
+
+        # Yo there's a result. Let's embed that.
+        embed = utils.Embed(use_random_colour=True)
+        outputs.sort(key=lambda i: (i[2], i[0]), reverse=True)
+        embed.description = ""
+        for key, url, weight in outputs:
+            v = f"[`{key}`]({url})\n"
+            if v in embed.description:
+                continue
+            embed.description += v
+            if embed.description.count("\n") >= 8:
+                break
+        return {"embed": embed}
+
     async def get_discordjs_docs(self):
         """
         Get the DiscordJS documentation from their Github page.
         """
-
-        if self.DISCORDJS_DOCS is not None:
-            return self.DISCORDJS_DOCS
 
         async with self.bot.session.get("https://raw.githubusercontent.com/discordjs/discord.js/docs/stable.json") as r:
             body = await r.text()
@@ -76,21 +133,37 @@ class LibraryDocs(utils.Cog):
         cache = {}
 
         for clazz in data['classes']:
-            cache[clazz['name']] = f"class/{clazz['name']}"
+            cache[clazz['name']] = f"https://discord.js.org/#/docs/main/stable/class/{clazz['name']}"
             for p in clazz.get('props', list()) + clazz.get('methods', list()):
                 if p['name'].startswith('_'):
                     continue
-                cache[f"{clazz['name']}.{p['name']}"] = f"class/{clazz['name']}?scrollTo={p['name']}"
+                cache[f"{clazz['name']}.{p['name']}"] = f"https://discord.js.org/#/docs/main/stable/class/{clazz['name']}?scrollTo={p['name']}"
 
         for clazz in data['typedefs']:
-            cache[clazz['name']] = f"typedef/{clazz['name']}"
+            cache[clazz['name']] = f"https://discord.js.org/#/docs/main/stable/typedef/{clazz['name']}"
             for p in clazz.get('props', list()) + clazz.get('methods', list()):
                 if p['name'].startswith('_'):
                     continue
-                cache[f"{clazz['name']}.{p['name']}"] = f"typedef/{clazz['name']}?scrollTo={p['name']}"
+                cache[f"{clazz['name']}.{p['name']}"] = f"https://discord.js.org/#/docs/main/stable/typedef/{clazz['name']}?scrollTo={p['name']}"
 
-        self.DISCORDJS_DOCS = cache
-        return self.DISCORDJS_DOCS
+        self._rtfm_cache['djs'] = cache
+
+    async def get_jda_docs(self):
+        """
+        Get the DiscordJS documentation from their Github page.
+        """
+
+        async with self.bot.session.get("https://ci.dv8tion.net/job/JDA/javadoc/member-search-index.js") as r:
+            body = await r.text()
+        data = json.loads(body.split("=", 1)[-1].strip())
+
+        cache = {}
+
+        for item in data:
+            cache[f"{item['c']}"] = f"https://ci.dv8tion.net/job/JDA/javadoc/{item['p'].replace('.', '/')}.html"
+            cache[f"{item['c']}.{item['l'].split('(')[0]}"] = f"https://ci.dv8tion.net/job/JDA/javadoc/{item['p'].replace('.', '/')}.html#{item['url']}"
+
+        self._rtfm_cache['jda'] = cache
 
     def parse_object_inv(self, stream, url):
         # key: URL
@@ -123,11 +196,6 @@ class LibraryDocs(utils.Cog):
             name, directive, prio, location, dispname = match.groups()
             domain, _, subdirective = directive.partition(':')
             if directive == 'py:module' and name in result:
-                # From the Sphinx Repository:
-                # due to a bug in 1.1 and below,
-                # two inventory entries are created
-                # for Python modules, and the first
-                # one is correct
                 continue
 
             # Most documentation pages have a label
@@ -150,36 +218,45 @@ class LibraryDocs(utils.Cog):
     async def build_rtfm_lookup_table(self, page_types):
         cache = {}
         for key, page in page_types.items():
-            sub = cache[key] = {}
+            cache[key] = {}
             async with self.bot.session.get(page + '/objects.inv') as resp:
                 if resp.status != 200:
                     raise RuntimeError('Cannot build rtfm lookup table, try again later.')
-
                 stream = SphinxObjectFileReader(await resp.read())
                 cache[key] = self.parse_object_inv(stream, page)
-
         self._rtfm_cache = cache
 
     async def do_rtfm(self, ctx, key, obj):
+        """
+        Check docs, generate and send output.
+        """
+
         page_types = {
             'latest': 'https://discordpy.readthedocs.io/en/latest',
             'python': 'https://docs.python.org/3',
             'pygame': 'https://www.pygame.org/docs',
             'voxelbotutils': 'https://voxelbotutils.readthedocs.io/en/latest/',
         }
+        non_sphinx_page_types = {
+            'djs': self.get_discordjs_docs,
+            'jda': self.get_jda_docs,
+        }
 
+        # Make sure we got something
         if obj is None:
-            await ctx.send(page_types[key])
             return
 
+        # See if we have an rtfm cache
         if not hasattr(self, '_rtfm_cache'):
             await ctx.trigger_typing()
             await self.build_rtfm_lookup_table(page_types)
+            for i in non_sphinx_page_types.values():
+                await i()
 
         obj = re.sub(r'^(?:discord\.(?:ext\.)?)?(?:commands\.)?(.+)', r'\1', obj)
 
-        if key.startswith('latest'):
-            # point the abc.Messageable types properly:
+        # point the discord.abc.Messageable types properly
+        if key == 'latest':
             q = obj.lower()
             for name in dir(discord.abc.Messageable):
                 if name[0] == '_':
@@ -188,47 +265,7 @@ class LibraryDocs(utils.Cog):
                     obj = f'abc.Messageable.{name}'
                     break
 
-        item_casefold = obj.casefold()
-        split = item_casefold.split('.')
-        outputs = []
-
-        for key, link in self._rtfm_cache[key].items():
-
-            # Key would be something like 'TextChannel.fetch', now 'textchannel.fetch'
-            key_casefold = key.casefold()
-
-            # See if we got an exact match
-            if item_casefold in key_casefold:
-                if item_casefold == key_casefold:
-                    outputs.append((key, link, 20,))
-                else:
-                    outputs.append((key, link, 15,))
-
-            # See if we're looking for a method
-            if len(split) == 1:
-                continue
-
-            # Search by method
-            if split[1] and split[1] in key_casefold:
-                if split[1] == key_casefold:
-                    outputs.append((key, link, 12,))
-                else:
-                    outputs.append((key, link, 10,))
-
-            # Search by class
-            if split[0] and split[0] in key_casefold:
-                if split[0] == key_casefold:
-                    outputs.append((key, link, 7,))
-                else:
-                    outputs.append((key, link, 5,))
-
-        embed = utils.Embed(use_random_colour=True)
-        if len(outputs) == 0:
-            return await ctx.send('Could not find anything. Sorry.')
-
-        outputs.sort(key=lambda i: (i[2], i[0]), reverse=True)
-        embed.description = '\n'.join(f'[`{key}`]({url})' for key, url, _ in outputs[:8])
-        await ctx.send(embed=embed)
+        return await ctx.send(**self.get_embed_from_cache(self._rtfm_cache[key], obj))
 
     @utils.group()
     async def rtfm(self, ctx):
