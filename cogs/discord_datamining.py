df5108bb9139b8840b27ed8fc3439ec8e99e2efc
SilverDimond <46320280+Silverdimond@users.noreply.github.com>
2021-01-13 15:07:16 +0100

    Make it look nicer

--- cogs/discord_datamining.py
+++ cogs/discord_datamining.py
@@ -74,13 +74,18 @@ class DiscordDatamining(utils.Cog):
                 data = await r.json()
             comment_text.extend([(i['commit_id'], re.sub(r"^## (.+)", r"**\1**", i['body'])) for i in data])
         self.logger.info(f"Logging info of {len(comment_text)} comments")
-        #get the description
+
+        # Get the full description
         descriptionunsplit =  '\n'.join([f"{i['commit']['message']} - [Link]({repo_url}commit/{i['sha']})" for i in new_commits])
+
+        # Inform the bot owner that the description is larger than allowed by discord
         if len(descriptionunsplit)>2048:
-            self.logger.info(f"So it turns out our description is longer than 2048 and this isnt really poggers, we need to make it in multiple embeds. It was {len(descriptionunsplit)}")
-        #make the description into a list of descriptions each 2048 or less
+            self.logger.info(f"Our description is longer than 2048 characters and this isnt really poggers, we need to make it in multiple embeds. It was {len(descriptionunsplit)} characters.")
+
+        # Split the description into a list of descriptions each 2048 or less
         descriptionarray = [descriptionunsplit[i:i+2048] for i in range(0, len(descriptionunsplit), 2048)]
-           # Format it all into an embed
+
+        # Format one into an embed
         with utils.Embed(use_random_colour=True) as embed:
             embed.title = embed_title
             embed.description = descriptionarray.pop()
@@ -91,13 +96,16 @@ class DiscordDatamining(utils.Cog):
         channel = self.bot.get_channel(self.VFL_CODING_CHANNEL_ID)
         m = await channel.send(embed=embed)
         await m.publish()
-        # if there are more than 2048 send the other parts
+
+        # If there are more than 2048 characters send the other parts
         for description in descriptionarray:
             with utils.Embed(use_random_colour=True) as newembed:
                 embed.title ="Continuation of "+ embed_title
                 embed.description = description
                 nm = await channel.send(embed=embed)
                 await nm.publish()
+
+        # We are done PogChamp
         self.logger.info("Sent data to channel")
         self.last_posted_commit[repo_url] = new_commits[0]['sha']
 
