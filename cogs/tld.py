52f1f0a37261a48a630e6be92eedf8fe233956fe
fadedmax <me@fadedmax.net>
2021-01-17 16:47:41 -0700

    Add tld check command

--- cogs/tld.py
+++ cogs/tld.py
@@ -39,7 +39,20 @@ class tld(utils.Cog):
         if domain is None:
             await ctx.send(tld)
         else:
-            await ctx.send(f"{domain}.{tld}")
+            await ctx.send(f"{domain}.{tld.lower()}")
+
+    @tlds.command()
+    async def check(self, ctx:utils.Context, tld_check:str=None):
+        """Checks if a tld exists"""
+
+        if self.tld_list is None:
+            await self.load_tlds()
+        tld_check = tld_check.upper()
+        if tld_check in self.tld_list:
+            await ctx.send(f"Is a tld. (.{tld_check.lower()}) <:tick_filled_yes:784976310366634034>")
+        else:
+            await ctx.send(f"Not a tld. (.{tld_check.lower()}) <:tick_filled_no:784976328231223306>")
+    
         
 
 
