ade301178ffa7607380f3111456124b92268b3ed
Callum Bartlett <callum@voxelfox.co.uk>
2021-01-18 20:32:51 +0000

    Move the gh/x/y command into the preexisting Github command cog

--- cogs/github_commands.py
+++ cogs/github_commands.py
@@ -16,6 +16,32 @@ class GithubCommands(utils.Cog):
         'filter': '!)Q29mwsOXXJEzlkx48kegp4T',
     }
 
+    @utils.Cog.listener()
+    async def on_message(self, message):
+        """
+        Sends GitHub/Lab links if a message sent in the server matches the format `gh/user/repo`.
+        """
+
+        # Find matches in the message
+        m = re.finditer(r'(?P<ident>g[hl])/(?P<url>(?P<user>\S{1,255})/(?P<repo>\S{1,255}))', message.content)
+
+        # Dictionary of possible Git() links
+        git_dict = {
+            "gh": "hub",
+            "gl": "lab"
+        }
+
+        # Add the url of each matched link to the final output
+        sendable = ""
+        for i in m:
+            url = i.group("url")
+            ident = i.group("ident")
+            sendable += f"<https://git{git_dict[ident]}.com/{url}>\n"
+
+        # Send the GitHub links if there's any output
+        if sendable:
+            await message.channel.send(sendable)
+
     @utils.Cog.listener("on_message")
     async def github_message_answer_listener(self, message:discord.Message):
         """Listens for messages being made and finds Github answer URLs in them"""
