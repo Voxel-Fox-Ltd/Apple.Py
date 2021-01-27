695092eca91e9da808bda7f0c3c03f592c961a19
Callum Bartlett <callum@voxelfox.co.uk>
2021-01-27 18:04:31 +0000

    Fix rows being undefined

--- cogs/github_commands.py
+++ cogs/github_commands.py
@@ -143,7 +143,7 @@ class GithubCommands(utils.Cog):
         }
         headers = {
             'Accept': 'application/vnd.github.v3+json',
-            'Authorization': f"token {rows[0]['github_access_token']}",
+            'Authorization': f"token {user_rows[0]['github_access_token']}",
         }
         async with self.bot.session.post(f"https://api.github.com/repos/{owner}/{repo}/issues", json=json, headers=headers) as r:
             data = await r.json()
