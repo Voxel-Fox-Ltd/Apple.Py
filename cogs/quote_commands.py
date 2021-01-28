9d932b1d527451e072c73fb55a80e9b5c0642312
JMcB#7918 <joel.mcbride1@live.com>
2021-01-28 10:43:30 +0000

    Support quoting with message replies using the .quote command.

--- cogs/quote_commands.py
+++ cogs/quote_commands.py
@@ -36,7 +36,10 @@ class QuoteCommands(utils.Cog):
 
         # Make sure a message was passed
         if not messages:
-            return await ctx.send("I couldn't find any references to messages in your command call.")
+            if ctx.message.reference is not None:
+                messages = [ctx.message.reference.message_id]
+            else:
+                return await ctx.send("I couldn't find any references to messages in your command call.")
 
         # Recreate the message list without duplicates
         unique_messages = []
