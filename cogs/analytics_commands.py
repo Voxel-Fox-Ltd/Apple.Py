c6ddd212bdce41b4d6a151b7db6a8ccaa9744cdd
JMcB <joel.mcbride1@live.com>
2021-02-15 19:34:42 +0000

    Fix minor bugs

--- cogs/analytics_commands.py
+++ cogs/analytics_commands.py
@@ -12,8 +12,8 @@ DISCORD_EMOTE_REGEX = re.compile(r'<:(\w*):\d*>')
 
 class AnalyticsCommands(utils.Cog):
     @utils.command()
-    @utils.cooldown.cooldown(1, 600, commands.BucketType.guild)
-    @commands.bot_has_permissions(read_message_history=True, send_messages=True, attach_file=True)
+    @utils.cooldown.cooldown(1, 6000, commands.BucketType.guild)
+    @commands.bot_has_permissions(read_message_history=True, send_messages=True, attach_files=True)
     def linkdump(self, ctx:utils.Context, target_channel:discord.TextChannel=None):
         """
         Get all links ever sent in a channel and dump them to a txt file.
@@ -26,7 +26,7 @@ class AnalyticsCommands(utils.Cog):
 
         await ctx.send('Processing...')
         all_links = []
-        for message in target_channel.history():
+        for message in target_channel.history(limit=None):
             for embed in message.embeds:
                 all_links.append(embed.url)
 
@@ -36,8 +36,8 @@ class AnalyticsCommands(utils.Cog):
                                   file=discord_file)
 
     @utils.command(name='analyse-emote-usage')
-    @utils.cooldown.cooldown(1, 600, commands.BucketType.guild)
-    @commands.bot_has_permissions(read_message_history=True, send_messages=True, attach_file=True)
+    @utils.cooldown.cooldown(1, 6000, commands.BucketType.guild)
+    @commands.bot_has_permissions(read_message_history=True, send_messages=True, attach_files=True)
     def emotes(self, ctx:utils.Context):
         """
         Get a csv containing the timestamp of every discord emote ever sent in a guild.
@@ -51,10 +51,11 @@ class AnalyticsCommands(utils.Cog):
         with io.StringIO as file_stream:
             csvw = csv.writer(file_stream)
             for channel in ctx.guild.channels:
-                for message in channel.history():
-                    emotes_used = re.findall(DISCORD_EMOTE_REGEX, message.content)
-                    message_timestamp = message.created_at.timestamp()
-                    csvw.writerows([[message_timestamp, emote_used] for emote_used in emotes_used])
+                if channel.type == discord.ChannelType.text:
+                    for message in channel.history(limit=None):
+                        emotes_used = re.findall(DISCORD_EMOTE_REGEX, message.content)
+                        message_timestamp = message.created_at.timestamp()
+                        csvw.writerows([[message_timestamp, emote_used] for emote_used in emotes_used])
 
             discord_file = discord.File(file_stream, filename='emotes.csv')
             return await ctx.send('Here are all the emotes used in this guild (server):', file=discord_file)
