f1ec144227d7bdca9c9405a6adbdfd75f0682132
JMcB <joel.mcbride1@live.com>
2021-02-15 19:15:01 +0000

    Add analyse-emote-usage command

--- cogs/analytics_commands.py
+++ cogs/analytics_commands.py
@@ -1,20 +1,26 @@
 import io
+import re
+import csv
 
 import discord
 from discord.ext import commands
 import voxelbotutils as utils
 
 
-class AnalyticsCommands(utils.Cog):
-    """
-    Get all links ever sent in a channel and dump them to a txt file.
+DISCORD_EMOTE_REGEX = re.compile(r'<:(\w*):\d*>')
 
-    Takes a channel as an argument or defaults to the channel in which the command was invoked.
-    """
+
+class AnalyticsCommands(utils.Cog):
     @utils.command()
     @utils.cooldown.cooldown(1, 600, commands.BucketType.guild)
     @commands.bot_has_permissions(read_message_history=True, send_messages=True, attach_file=True)
     def linkdump(self, ctx:utils.Context, target_channel:discord.TextChannel=None):
+        """
+        Get all links ever sent in a channel and dump them to a txt file.
+
+        Takes a channel as an argument or defaults to the channel in which the command was invoked.
+        """
+        # todo: add `regex` argument takes a regular expression and only returns links that match
         if target_channel is None:
             target_channel = ctx.channel
 
@@ -24,9 +30,34 @@ class AnalyticsCommands(utils.Cog):
             for embed in message.embeds:
                 all_links.append(embed.url)
 
-        file_stream = io.StringIO('\n'.join(all_links))
-        discord_file = discord.File(file_stream, filename='links.txt')
-        await ctx.send(f'Here are all the links ever sent in the {target_channel.name} channel:', file=discord_file)
+        with io.StringIO('\n'.join(all_links)) as file_stream:
+            discord_file = discord.File(file_stream, filename='links.txt')
+            return await ctx.send(f'Here are all the links ever sent in the {target_channel.name} channel:',
+                                  file=discord_file)
+
+    @utils.command(name='analyse-emote-usage')
+    @utils.cooldown.cooldown(1, 600, commands.BucketType.guild)
+    @commands.bot_has_permissions(read_message_history=True, send_messages=True, attach_file=True)
+    def emotes(self, ctx:utils.Context):
+        """
+        Get a csv containing the timestamp of every discord emote ever sent in a guild.
+
+        The user can take this and make a graph or whatever. Useful for figuring out which emotes to remove
+        when you're at the cap!
+        """
+        # todo: add option to give channels to analyse as arguments, instead of analysing all in guild
+        await ctx.send('Processing...')
+
+        with io.StringIO as file_stream:
+            csvw = csv.writer(file_stream)
+            for channel in ctx.guild.channels:
+                for message in channel.history():
+                    emotes_used = re.findall(DISCORD_EMOTE_REGEX, message.content)
+                    message_timestamp = message.created_at.timestamp()
+                    csvw.writerows([[message_timestamp, emote_used] for emote_used in emotes_used])
+
+            discord_file = discord.File(file_stream, filename='emotes.csv')
+            return await ctx.send('Here are all the emotes used in this guild (server):', file=discord_file)
 
 
 def setup(bot:utils.Bot):
