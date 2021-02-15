9af05f88797fd336f6bafc7d3baf343599475da9
JMcB <joel.mcbride1@live.com>
2021-02-15 19:49:14 +0000

    Adjust arguments for analytics commands
    add option to give channels to analyse as arguments - instead of analysing all in guild, for analyse-emote-usage, and add option to pass multiple channels to linkdump

--- cogs/analytics_commands.py
+++ cogs/analytics_commands.py
@@ -14,21 +14,22 @@ class AnalyticsCommands(utils.Cog):
     @utils.command()
     @utils.cooldown.cooldown(1, 6000, commands.BucketType.guild)
     @commands.bot_has_permissions(read_message_history=True, send_messages=True, attach_files=True)
-    def linkdump(self, ctx:utils.Context, target_channel:discord.TextChannel=None):
+    def linkdump(self, ctx:utils.Context, target_channels:commands.Greedy[discord.TextChannel]=None):
         """
         Get all links ever sent in a channel and dump them to a txt file.
 
-        Takes a channel as an argument or defaults to the channel in which the command was invoked.
+        Takes channels as arguments, or defaults to the channel in which the command was invoked.
         """
         # todo: add `regex` argument takes a regular expression and only returns links that match
-        if target_channel is None:
-            target_channel = ctx.channel
+        if target_channels is None:
+            target_channels = [ctx.channel]
 
         await ctx.send('Processing...')
         all_links = []
-        for message in target_channel.history(limit=None):
-            for embed in message.embeds:
-                all_links.append(embed.url)
+        for target_channel in target_channels:
+            for message in target_channel.history(limit=None):
+                for embed in message.embeds:
+                    all_links.append(embed.url)
 
         with io.StringIO('\n'.join(all_links)) as file_stream:
             discord_file = discord.File(file_stream, filename='links.txt')
@@ -38,19 +39,22 @@ class AnalyticsCommands(utils.Cog):
     @utils.command(name='analyse-emote-usage')
     @utils.cooldown.cooldown(1, 6000, commands.BucketType.guild)
     @commands.bot_has_permissions(read_message_history=True, send_messages=True, attach_files=True)
-    def emotes(self, ctx:utils.Context):
+    def emotes(self, ctx:utils.Context, target_channels:commands.Greedy[discord.TextChannel]=None):
         """
         Get a csv containing the timestamp of every discord emote ever sent in a guild.
 
+        Takes channels as arguments, or defaults to every channel in the guild.
         The user can take this and make a graph or whatever. Useful for figuring out which emotes to remove
         when you're at the cap!
         """
-        # todo: add option to give channels to analyse as arguments, instead of analysing all in guild
+        if target_channels is None:
+            target_channels = ctx.guild.channels
+
         await ctx.send('Processing...')
 
         with io.StringIO as file_stream:
             csvw = csv.writer(file_stream)
-            for channel in ctx.guild.channels:
+            for channel in target_channels:
                 if channel.type == discord.ChannelType.text:
                     for message in channel.history(limit=None):
                         emotes_used = re.findall(DISCORD_EMOTE_REGEX, message.content)
