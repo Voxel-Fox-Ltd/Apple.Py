64803f62f6053d773bc6eb857368695a9324380d
Caity-chan <74330895+Caity-chan@users.noreply.github.com>
2021-02-17 09:43:54 -0800

    made bot more confident
    changed "should be" to "estimated to be around"

--- cogs/timezone_info.py
+++ cogs/timezone_info.py
@@ -63,7 +63,7 @@ class TimezoneInfo(utils.Cog):
                 DO UPDATE SET timezone_offset=excluded.timezone_offset""",
                 ctx.author.id, total_minute_offset,
             )
-        await ctx.send(f"I think you're somewhere around **UTC{hour_offset:=+03d}:{minute_offset:=02d}** - I've stored this in the database.")
+        await ctx.send(f"Your time is estimated to be around **UTC{hour_offset:=+03d}:{minute_offset:=02d}** - I've stored this in the database.")
 
     @timezone.command(name="get")
     async def timezone_get(self, ctx:utils.Context, user:discord.Member=None):
@@ -82,7 +82,7 @@ class TimezoneInfo(utils.Cog):
         if not rows or rows[0]['timezone_offset'] is None:
             return await ctx.send(f"{user.mention} hasn't set up their timezone information! They can set it with `{ctx.clean_prefix}timezone set`.")
         formatted_time = (dt.utcnow() + timedelta(minutes=rows[0]['timezone_offset'])).strftime('%-I:%M %p')
-        await ctx.send(f"The current time for {user.mention} _should_ be somewhere around **{formatted_time}**.", allowed_mentions=discord.AllowedMentions.none())
+        await ctx.send(f"The current time for {user.mention} is estimated to be around **{formatted_time}**.", allowed_mentions=discord.AllowedMentions.none())
 
 
 def setup(bot:utils.Bot):
