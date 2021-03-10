ef4b5b3b55956e4567da377da3fe631aba8cbc00
Kae Bartlett <Kae@voxelfox.co.uk>
2021-03-10 04:42:39 +0000

    Allow the google command to be paginated

--- cogs/google_commands.py
+++ cogs/google_commands.py
@@ -3,6 +3,30 @@ import voxelbotutils as utils
 
 class GoogleCommands(utils.Cog):
 
+    def get_search_page(self, query:str):
+        """
+        Get a number of results from Google
+        """
+
+        async def wrapper(page_number:int):
+            params = {
+                'key': self.bot.config['api_keys']['google']['api_key'],
+                'cx': self.bot.config['api_keys']['google']['search_engine_id'],
+                'num': 3,
+                'q': query,
+                'safe': 'active',
+                'start': 3 * page_number,
+            }
+            async with self.bot.session.get("https://customsearch.googleapis.com/customsearch/v1", params=params) as r:
+                data = await r.json()
+            ENDL = '\n'
+            output_data = []
+            for d in data['items']:
+                output_data.append((d['title'][:256], f"[{d['displayLink']}]({d['link']}) - {d['snippet'].replace(ENDL, ' ')}"))
+            return output_data
+
+        return wrapper
+
     @utils.command()
     @utils.checks.is_config_set('api_keys', 'google', 'search_engine_id')
     @utils.checks.is_config_set('api_keys', 'google', 'api_key')
@@ -11,20 +35,12 @@ class GoogleCommands(utils.Cog):
         Search a query on Google.
         """
 
-        params = {
-            'key': self.bot.config['api_keys']['google']['api_key'],
-            'cx': self.bot.config['api_keys']['google']['search_engine_id'],
-            'num': 5,
-            'q': query,
-            'safe': 'active',
-        }
-        async with self.bot.session.get("https://customsearch.googleapis.com/customsearch/v1", params=params) as r:
-            data = await r.json()
-        with utils.Embed(use_random_colour=True) as embed:
-            ENDL = '\n'
-            for d in data['items']:
-                embed.add_field(d['title'][:256], f"[{d['displayLink']}]({d['link']}) - {d['snippet'].replace(ENDL, ' ')}", inline=False)
-        await ctx.send(embed=embed)
+        def formatter(menu, data):
+            embed = utils.Embed(use_random_colour=True)
+            for d in data:
+                embed.add_field(*d, inline=False)
+            return embed
+        await utils.Paginator(self.get_search_page(query), formatter=formatter).start(ctx)
 
 
 def setup(bot:utils.Bot):
