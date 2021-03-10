9d71053832f4c165a3deeb9a58ad8c9d99f5dfe4
Kae Bartlett <kae@voxelfox.co.uk>
2021-03-10 07:48:06 +0000

    Change up some whitespace and comments in the movie commands cog

--- cogs/movie_commands.py
+++ cogs/movie_commands.py
@@ -5,13 +5,17 @@ import voxelbotutils as utils
 
 class MovieCommand(utils.Cog):
 
-    async def omdb_query(self, name, mediatype, search, year=None):
+    async def send_omdb_query(self, name:str, media_type:str, search:bool, year=None) -> dict:
+        """
+        Send a query to the OMDB API, returning the results dict.
+        """
+
         # Build up the params
         key = 's' if search else 't'
         params = {
             'apikey': self.bot.config['api_keys']['omdb'],
+            'type': media_type,
             key: name,
-            'type': mediatype
         }
         if year:
             params.update({'year': year})
@@ -21,20 +25,22 @@ class MovieCommand(utils.Cog):
         async with self.bot.session.get("http://www.omdbapi.com/", params=params, headers=headers) as r:
             data = await r.json()
         return data
-    
-    def generate_embed(self,data):
+
+    def generate_embed(self, data) -> utils.Embed:
+        """
+        Make an embed based on some OMDB data.
+        """
+
         search = data.get('Title') is None
-        
         if search and data.get('Search') is None:
             return None
 
+        embed = utils.Embed(use_random_colour=True)
+        if not search:
+            embed.title = f"{data['Title']} ({data['Year']})"
+
+        # List short details of up to 10 results
         if search:
-            embed = utils.Embed(use_random_colour=True)
-        else:
-            embed = utils.Embed(use_random_colour=True, title=f"{data['Title']} ({data['Year']})")
-        
-        if search:
-            # List short details of up to 10 results
             description_list = []
             for index, row in enumerate(data['Search'][:10], start=1):
                 if row['Poster']:
@@ -42,30 +48,31 @@ class MovieCommand(utils.Cog):
                 else:
                     description_list.append(f"{index}. **{row['Title']}** ({row['Year']})")
             embed.description = '\n'.join(description_list)
-        else:
-            # List full details
-            if data.get('Plot'):
-                embed.description = data['Plot']
-            if data.get('Released'):
-                embed.add_field("Release Date", data['Released'])
-            if data.get('Rated'):
-                embed.add_field("Age Rating", data['Rated'])
-            if data.get('Runtime'):
-                embed.add_field("Runtime", data['Runtime'])
-            if data.get('Genre'):
-                embed.add_field("Genre", data['Genre'])
-            if data.get('imdbRating'):
-                embed.add_field("IMDB Rating", data['imdbRating'])
-            if data.get('Production'):
-                embed.add_field("Production Company", data['Production'])
-            if data.get('Director'):
-                embed.add_field("Director", data['Director'])
-            if data.get('Writer'):
-                embed.add_field("Writer", data['Writer'], inline=False)
-            if data.get('imdbID'):
-                embed.add_field("IMDB Page", f"[Direct Link](https://www.imdb.com/title/{data['imdbID']}/) - IMDB ID `{data['imdbID']}`", inline=False)
-            if data.get('Poster'):
-                embed.set_thumbnail(data['Poster'])
+            return embed
+
+        # List full details
+        if data.get('Plot'):
+            embed.description = data['Plot']
+        if data.get('Released'):
+            embed.add_field("Release Date", data['Released'])
+        if data.get('Rated'):
+            embed.add_field("Age Rating", data['Rated'])
+        if data.get('Runtime'):
+            embed.add_field("Runtime", data['Runtime'])
+        if data.get('Genre'):
+            embed.add_field("Genre", data['Genre'])
+        if data.get('imdbRating'):
+            embed.add_field("IMDB Rating", data['imdbRating'])
+        if data.get('Production'):
+            embed.add_field("Production Company", data['Production'])
+        if data.get('Director'):
+            embed.add_field("Director", data['Director'])
+        if data.get('Writer'):
+            embed.add_field("Writer", data['Writer'], inline=False)
+        if data.get('imdbID'):
+            embed.add_field("IMDB Page", f"[Direct Link](https://www.imdb.com/title/{data['imdbID']}/) - IMDB ID `{data['imdbID']}`", inline=False)
+        if data.get('Poster'):
+            embed.set_thumbnail(data['Poster'])
         return embed
 
     @utils.group(invoke_without_command=True)
@@ -84,13 +91,11 @@ class MovieCommand(utils.Cog):
         else:
             year = None
 
-        data = await self.omdb_query(name,'movie',False,year)
-
+        # Try and return the found data
+        data = await self.send_omdb_query(name,'movie',False,year)
         embed = self.generate_embed(data)
-
-        if not embed:
+        if embed is None:
             return await ctx.invoke(self.bot.get_command("movie search"), name=original_name)
-
         return await ctx.send(embed=embed)
 
     @movie.group(name="search")
@@ -109,13 +114,11 @@ class MovieCommand(utils.Cog):
         else:
             year = None
 
-        data = await self.omdb_query(name,'movie',True,year)
-
+        # Try and return the found data
+        data = await self.send_omdb_query(name,'movie',True,year)
         embed = self.generate_embed(data)
-
         if not embed:
-            return await ctx.send(f"No movie results for `{original_name}` could be found.", allowed_mentions=discord.AllowedMentions(users=False, roles=False, everyone=False))
-
+            return await ctx.send(f"No movie results for `{original_name}` could be found.", allowed_mentions=discord.AllowedMentions.none())
         return await ctx.send(embed=embed)
 
     @utils.group(invoke_without_command=True)
@@ -134,16 +137,14 @@ class MovieCommand(utils.Cog):
         else:
             year = None
 
-        data = await self.omdb_query(name,'series',False,year)
-
+        # Try and return the found data
+        data = await self.send_omdb_query(name, 'series', False, year)
         embed = self.generate_embed(data)
-
-        if not embed:
+        if embed is None:
             return await ctx.invoke(self.bot.get_command("tv search"), name=original_name)
-        
         return await ctx.send(embed=embed)
-    
-    @tv.group(name="search")
+
+    @tv.command(name="search")
     @commands.bot_has_permissions(send_messages=True, embed_links=True)
     @utils.checks.is_config_set('api_keys', 'omdb')
     async def tv_search(self, ctx:utils.Context, *, name:str):
@@ -159,13 +160,11 @@ class MovieCommand(utils.Cog):
         else:
             year = None
 
-        data = await self.omdb_query(name,'series',True,year)
-
+        # Try and return the found data
+        data = await self.send_omdb_query(name, 'series', True, year)
         embed = self.generate_embed(data)
-
         if not embed:
-            return await ctx.send(f"No TV series results for `{original_name}` could be found.", allowed_mentions=discord.AllowedMentions(users=False, roles=False, everyone=False))
-
+            return await ctx.send(f"No TV series results for `{original_name}` could be found.", allowed_mentions=discord.AllowedMentions.none())
         return await ctx.send(embed=embed)
 
 
