a94f74cc728c29442a3479b25363039ece8dff41
George M <32426140+iHeroGH@users.noreply.github.com>
2021-03-14 19:11:04 -0700

    Moved search alias from wolfram to google (#135)
    * Added a search alias to the google group
    
    * Removed search alias from wolfram

--- cogs/wolfram_alpha.py
+++ cogs/wolfram_alpha.py
@@ -6,7 +6,7 @@ import voxelbotutils as utils
 
 class WolframAlpha(utils.Cog):
 
-    @utils.command(aliases=['wf', 'wolframalpha', 'wfa', 'search'])
+    @utils.command(aliases=['wf', 'wolframalpha', 'wfa'])
     @commands.bot_has_permissions(send_messages=True, embed_links=True)
     @utils.checks.is_config_set('api_keys', 'wolfram')
     async def wolfram(self, ctx, *, search:str):
