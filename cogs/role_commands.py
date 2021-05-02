447ba314bf87108296f5329bb2fe92495fe93bdc
Kae Bartlett <Kae@voxelfox.co.uk>
2021-05-02 05:47:30 +0100

    Add dumpmessageembeds command

--- cogs/role_commands.py
+++ cogs/role_commands.py
@@ -1,4 +1,5 @@
 import typing
+import json
 
 import discord
 from discord.ext import commands
@@ -67,6 +68,16 @@ class RoleCommands(utils.Cog):
         """
 
         await ctx.send(f'```\n{message.content}\n```')
+
+    @utils.command(aliases=['dumpmessageembed'])
+    @commands.has_permissions(manage_messages=True)
+    @commands.bot_has_permissions(send_messages=True)
+    async def dumpmessageembeds(self, ctx:utils.Context, message:discord.Message):
+        """
+        Dumps a message out into chat.
+        """
+
+        await ctx.send(f'```json\n{json.dumps([i.to_json() for i in message.embeds], indent=2)}\n```')
 
     @commands.command(cls=utils.Command)
     @commands.has_permissions(manage_messages=True)
