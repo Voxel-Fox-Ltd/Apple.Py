7d8e2aba9948405eeb5d30e1a6a95e4d309f79d3
Kae Bartlett <Kae@voxelfox.co.uk>
2021-05-13 20:15:52 +0100

    Fix staff pinger for new embed format

--- cogs/staff_pinger.py
+++ cogs/staff_pinger.py
@@ -5,14 +5,21 @@ class StaffPinger(utils.Cog):
 
     @utils.Cog.listener()
     async def on_message(self, message):
+        """
+        Pings the staff members when a report comes through.
+        """
+
         if message.channel.id != 709608383216353280:
             return
         if message.author.id != 204255221017214977:
             return
         await message.delete()
+        embed = None
+        if message.embeds:
+            embed = message.embeds[0]
         await message.channel.send(
             f"{message.content} <@&480519382699868181> <@&713026585569263637>",
-            # allowed_mentions=discord.AllowedMentions(users=False)
+            embed=embed,
         )
 
 
