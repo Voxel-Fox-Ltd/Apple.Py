7ce1405bdc0557cf8194dd58819962945c8a1c73
Kae Bartlett <kae@voxelfox.co.uk>
2021-08-11 11:27:43 +0100

    Add some more newlines to the csupport command

--- cogs/faq_handler.py
+++ cogs/faq_handler.py
@@ -2,7 +2,7 @@ import discord
 import voxelbotutils as vbu
 
 
-CSUPPORT_MESSAGE = """\u200b\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
+CSUPPORT_MESSAGE = ("\u200b\n" * 28) + """
 Please give a detailed report of:
 * What you thought would happen vs what actually happened
 * How you cause the issue to happen
