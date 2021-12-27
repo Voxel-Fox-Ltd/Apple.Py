c7e980256872dd7551f1fc777ea12a3435787a2f
Kae Bartlett <kae@voxelfox.co.uk>
2021-12-27 02:23:54 +0000

    Add new loggers to yags youtube message

--- cogs/yags_youtube_message.py
+++ cogs/yags_youtube_message.py
@@ -17,18 +17,22 @@ class YagsYoutubeMessage(vbu.Cog):
         if message.guild is None or message.guild.id != self.GUILD_ID:
             return
         if message.author.id != self.YAGS_USER_ID:
+            self.logger.info("Message in stream updates channel - not posted by Yags.")
             return
-        if "**Kae** uploaded a new youtube video" not in message.content and "Woah! **Kae** is currently streaming" not in message.content:
+        if "**Kae** uploaded a new youtube video" not in message.content and "**Kae** is currently streaming" not in message.content:
+            self.logger.info("Message in stream updates channel - not a notification.")
             return
         role = message.guild.get_role(self.YOUTUBE_UPDATE_ROLE)
         if role is None:
+            self.logger.info("Message in stream updates channel - could not get role.")
             return
         mentionable = role.mentionable
         await role.edit(mentionable=True)
-        m = await message.channel.send(f'{role.mention} ' + message.content)
+        m = await message.channel.send(f'{role.mention} {message.content}')
         await message.delete()
         await m.publish()
         await role.edit(mentionable=mentionable)
+        self.logger.info("Sent new message in stream updates channel.")
 
 
 def setup(bot: vbu.Bot):
