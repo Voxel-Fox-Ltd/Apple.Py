a04547bff46f40cdbb3cbf468107b17f1f5ce95a
Kae Bartlett <kae@voxelfox.co.uk>
2022-06-02 17:47:07 +0100

    Update some typing

--- cogs/types/bot.py
+++ cogs/types/bot.py
@@ -1,10 +1,9 @@
 from typing import Dict
 
 from discord.ext import vbu
-from voxelbotutils.cogs.utils.types.config import BotConfig
 
 
-class BotConfig(BotConfig):
+class BotConfig(vbu.types.BotConfig):
     api_keys: Dict[str, str]
 
 
