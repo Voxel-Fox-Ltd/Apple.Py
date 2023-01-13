0aafc792fbe54040e44e9c7f37870827f9436ab3
Kae Bartlett <kae@voxelfox.co.uk>
2023-01-13 10:34:26 +0000

    Update the steal sticker command

--- cogs/sticker_stealer.py
+++ cogs/sticker_stealer.py
@@ -7,44 +7,37 @@ from discord.ext import commands, vbu
 class StickerStealer(vbu.Cog):
 
     @commands.context_command(name="Steal This Sticker")
-    async def _context_command_steal_sticker(self, ctx: vbu.SlashContext, message: discord.Message):
-        command = self.steal_sticker
-        await command.can_run(ctx)
-        await ctx.invoke(command, message)
+    async def _context_command_steal_sticker(
+            self,
+            ctx: vbu.SlashContext,
+            message: discord.Message):
 
-    @commands.command(name="stealsticker")
-    @commands.has_permissions(manage_guild=True)
-    @commands.guild_only()
-    async def steal_sticker(self, ctx: vbu.Context, message: discord.Message):
-        """
-        Steal a sticker and add to the current server.
-        """
+        await commands.has_permissions(manage_guild=True).predicate(ctx)
+        await commands.guild_only().predicate(ctx)
 
-        assert ctx.guild
         if message.stickers and len(message.stickers) > 0:
-            sticker = message.stickers[0]
-            try:
-                await ctx.guild.create_sticker(
-                    name=sticker.name,
-                    description=sticker.name,
-                    emoji="::",
-                    file=discord.File(BytesIO(await sticker.read())),
-                )
-                if isinstance(ctx, vbu.SlashContext):
-                    return await ctx.send(
-                        f"Successfully created the sticker: {sticker.name}.", ephemeral=True
-                    )
-                await ctx.send(f"Successfully created the sticker: {sticker.name}.")
-            except (discord.Forbidden, discord.HTTPException) as e:
-                if isinstance(ctx, vbu.SlashContext):
-                    return await ctx.send(
-                        f"Failed to create the sticker due to: {e}", ephemeral=True
-                    )
-                await ctx.send(f"Failed to create the sticker due to: {e}")
+            pass
         else:
-            if isinstance(ctx, vbu.SlashContext):
-                return await ctx.send("This message does not have a sticker.", ephemeral=True)
-            await ctx.send("This message does not have a sticker.")
+            return await ctx.send(
+                "This message does not have a sticker.",
+                ephemeral=True,
+            )
+
+        sticker = message.stickers[0]
+        try:
+            new_sticker = await ctx.guild.create_sticker(
+                name=sticker.name,
+                description=sticker.name,
+                emoji="::",
+                file=discord.File(BytesIO(await sticker.read())),
+            )
+        except (discord.Forbidden, discord.HTTPException) as e:
+            return await ctx.send(
+                f"Failed to create the sticker due to: {e}",
+                ephemeral=True
+            )
+        else:
+            await ctx.send(f"Successfully created the sticker: {new_sticker.name}.")
 
 
 def setup(bot: vbu.Bot):
