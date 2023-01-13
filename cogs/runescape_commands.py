dc5684f7242846f2b39deb6c22c5a49b456227f2
Kae Bartlett <kae@voxelfox.co.uk>
2023-01-13 10:24:28 +0000

    Update some codestyle for runescape commands cog

--- cogs/runescape_commands.py
+++ cogs/runescape_commands.py
@@ -5,7 +5,6 @@ from pathlib import Path
 
 import discord
 from discord.ext import commands, vbu
-import aiohttp
 
 
 API_BASE_URL = 'https://secure.runescape.com/m=itemdb_oldschool/'
@@ -19,7 +18,12 @@ class RunescapeCommands(vbu.Cog):
 
     def __init__(self, bot):
         super().__init__(bot)
-        self.item_ids_path = Path().parent.joinpath('config').joinpath('osrs-item-ids.json')
+        self.item_ids_path = (
+            Path()
+            .parent
+            .joinpath('config')
+            .joinpath('osrs-item-ids.json')
+        )
         with open(self.item_ids_path) as item_ids_file:
             self.item_ids = json.load(item_ids_file)
 
@@ -59,17 +63,16 @@ class RunescapeCommands(vbu.Cog):
         headers = {
             "User-Agent": self.bot.user_agent,
         }
-        async with self.bot.session.get(url, params=params, headers=headers) as response:
-            # The Runescape API doesn't set a json header, so aiohttp complains about it.
-            # We can just say to not check the content type.
-            # https://github.com/aio-libs/aiohttp/blob/8c82ba11b9e38851d75476d261a1442402cc7592/aiohttp/web_request.py#L664-L681
-            item = await response.json(content_type=None)
+        resp = await self.bot.session.get(url, params=params, headers=headers)
+        # The Runescape API doesn't set a json header, so aiohttp complains
+        # about it. We can just say to not check the content type.
+        item = await resp.json(content_type=None)
+        return item['item']
 
-        # revolver ocelot (revolver ocelot)
-        item = item['item']
-        return item
-
-    async def parse_item_value(self, item: dict, return_int: bool = True) -> typing.Union[int, str]:
+    async def parse_item_value(
+            self,
+            item: dict,
+            return_int: bool = True) -> int | str:
         """
         Parse the value of an item from the JSON response from the rs API.
         """
@@ -81,7 +84,6 @@ class RunescapeCommands(vbu.Cog):
                 value = self.rs_notation_to_int(value)
         else:
             value = str(value)
-
         return value
 
     @commands.command(
@@ -101,30 +103,32 @@ class RunescapeCommands(vbu.Cog):
         Get the value of an item on the grand exchange (OSRS).
         """
 
-        async with ctx.typing():
-            if item.lower() in ['random']:
-                item_id = random.choice(list(self.item_ids.values()))
-            else:
-                item = item.capitalize()
-                item_id = self.item_ids.get(item)
+        if item.lower() in ['random']:
+            item_id = random.choice(list(self.item_ids.values()))
+        else:
+            item = item.capitalize()
+            item_id = self.item_ids.get(item)
 
-            if item_id:
-                item_dict = await self.get_item_details_by_id(item_id)
-                item_value = await self.parse_item_value(item_dict, return_int=False)
+        if not item_id:
+            return await ctx.send('Item not found')
 
-                name = item_dict['name']
-                item_page_url = API_BASE_URL + f"a=373/{name.replace(' ', '+')}/viewitem?obj={item_id}"
+        typing = await ctx.typing().__aenter__()
+        item_dict = await self.get_item_details_by_id(item_id)
+        item_value = await self.parse_item_value(item_dict, return_int=False)
 
-                with vbu.Embed() as embed:
-                    embed.set_author(name=name, url=item_page_url, icon_url=item_dict['icon'])
-                    embed.set_thumbnail(url=item_dict['icon_large'])
-                    embed.add_field('Value', f'{item_value} coins', inline=False)
-                    embed.add_field(f'Examine {name}', item_dict['description'], inline=False)
-                    embed.add_field('Members', MEMBERS_MAPPING[item_dict['members']], inline=False)
+        name = item_dict['name']
+        item_page_url = API_BASE_URL + f"a=373/{name.replace(' ', '+')}/viewitem?obj={item_id}"
 
-                return await ctx.send(embed=embed)
-            else:
-                return await ctx.send('Item not found')
+        with vbu.Embed() as embed:
+            embed.set_author(name=name, url=item_page_url, icon_url=item_dict['icon'])
+            embed.set_thumbnail(url=item_dict['icon_large'])
+            embed.add_field('Value', f'{item_value} coins', inline=False)
+            embed.add_field(f'Examine {name}', item_dict['description'], inline=False)
+            embed.add_field('Members', MEMBERS_MAPPING[item_dict['members']], inline=False)
+
+        await typing.__aexit__(None, None, None)
+
+        return await ctx.send(embed=embed)
 
 
 def setup(bot: vbu.Bot):
